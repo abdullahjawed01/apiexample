dcb2b5d501e6f6e11e174734a3d27c6ecd445453
abdullahjawed01 <abdullahjawed2021@gmail.com>
2025-11-29 16:11:11 +0530

    pushed

--- server/utils/twilio.js
+++ server/utils/twilio.js
@@ -1,7 +1,6 @@
 import twilio from 'twilio';
 
-const accountSid = "AC604a9df47ce55475b71bfc8b70b95f0c"
-const authToken = "4b28c8637c7873b903e84a058e68b0bc"
+
 const client = twilio(accountSid, authToken);
 
 client.messages
